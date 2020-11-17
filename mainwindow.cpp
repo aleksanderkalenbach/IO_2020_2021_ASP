b06fee29e41b5b07c52b6cffb2c97bffc5341843
Przemysław Pułanecki <ul0240460@edu.uni.lodz.pl>
2020-11-17 22:37:51 +0100

    Naprawa ikonek v2

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -17,16 +17,16 @@ MainWindow::MainWindow(QWidget *parent)
 
   QToolBar *toolbar = addToolBar("Main toolbar");
   toolbar->setIconSize(QSize(128,32));
-  toolbar->addAction(QIcon("/Users/aleks/OneDrive/Dokumenty/IO_20_21/icons/house.png"),"Start");
+  toolbar->addAction(QIcon(":/icon/house.png"),"Start");
   toolbar->addSeparator();
-  toolbar->addAction(QIcon("/Users/aleks/OneDrive/Dokumenty/IO_20_21/icons/users_5.png"),"Grupy");
-  toolbar->addAction(QIcon("/Users/aleks/OneDrive/Dokumenty/IO_20_21/icons/calendar_add.png"),"Zajecia");
+  toolbar->addAction(QIcon(":/icon/users_5.png"),"Grupy");
+  toolbar->addAction(QIcon(":/icon/calendar_add.png"),"Zajecia");
   toolbar->addSeparator();
-  toolbar->addAction(QIcon("/Users/aleks/OneDrive/Dokumenty/IO_20_21/icons/client_account_template.png"),"Kadry");
-  toolbar->addAction(QIcon("/Users/aleks/OneDrive/Dokumenty/IO_20_21/icons/coins.png"),"Ksiegowosc");
-  toolbar->addAction(QIcon("/Users/aleks/OneDrive/Dokumenty/IO_20_21/icons/user_oldman.png"),"Zarzad");
+  toolbar->addAction(QIcon(":/icon/client_account_template.png"),"Kadry");
+  toolbar->addAction(QIcon(":/icon/coins.png"),"Ksiegowosc");
+  toolbar->addAction(QIcon(":/icon/user_oldman.png"),"Zarzad");
   toolbar->addSeparator();
-  toolbar->addAction(QIcon("/Users/aleks/OneDrive/Dokumenty/IO_20_21/icons/wyjscie.png"),"Zamknij");
+  toolbar->addAction(QIcon(":/icon/wyjscie.png"),"Zamknij");
 
 
 }
