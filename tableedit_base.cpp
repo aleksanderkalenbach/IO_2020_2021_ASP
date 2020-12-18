a2b6435d7c22ac66e6ace5cc6284185180853250
Krispers <krispers.studios@gmail.com>
2020-12-18 12:30:25 +0100

    Dodanie edytowania rekordow uczestnikow

--- tableedit_base.cpp
+++ tableedit_base.cpp
@@ -64,6 +64,39 @@ void tableedit_base::on_pushButton_2_clicked()
     }
 }
 
+void tableedit_base::on_pushButton_3_clicked()
+{
+    int wiersz = ui->tableWidget->currentRow();
+    int id = ui->tableWidget->model()->index(wiersz,0).data().toInt();
+    QString Imie = ui->tableWidget->model()->index(wiersz,1).data().toString();
+    QString Nazwisko = ui->tableWidget->model()->index(wiersz,2).data().toString();
+    QDateTime Data_urodzenia = ui->tableWidget->model()->index(wiersz,3).data().toDateTime();
+    QString Email = ui->tableWidget->model()->index(wiersz,4).data().toString();
+    QString servername = "LOCALHOST";
+    QString dbname = "szkolaPlywacka";
+    QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
+    db.setConnectOptions();
+    QString dsn = QString("DRIVER={SQL SERVER};SERVER=%1;DATABASE=%2;Trusted_Connection=Yes;UID=test;PWD=test;").arg(servername).arg(dbname);
+    db.setDatabaseName(dsn);
+    if(db.open())
+    {
+        qDebug() << "open";
+        QSqlQuery query;
+        query.prepare("UPDATE [Uczestnik] SET Imie=:Imie, Nazwisko=:Nazwisko, Data_urodzenia=:Data_urodzenia, Email=:Email WHERE ID_Uczestnicy = :id");
+        query.bindValue(0,Imie);
+        query.bindValue(":Nazwisko",Nazwisko);
+        query.bindValue(":Data_urodzenia",Data_urodzenia);
+        query.bindValue(":Email",Email);
+        query.bindValue(":id", id);
+        query.exec();
+        db.close();
+    }
+    else
+    {
+        qDebug() << "error = " << db.lastError();
+    }
+}
+
 void tableedit_base::on_pushButton_4_clicked()//funkcja odświeżenia rekordów
 {
     ui->tableWidget->setRowCount(0);
@@ -93,3 +126,5 @@ void tableedit_base::on_pushButton_4_clicked()//funkcja odświeżenia rekordów
         qDebug() << "error = " << db.lastError();
     }
 }
+
+
